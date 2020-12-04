b9434d39715131e349327a7d6424ea23cbe25d92
alecthibodeau <alecthibodeau@gmail.com>
2020-12-04 06:18:12 -0500

    Complete 2.7 Variables & Variable Types

---
+++ section-02-07.py
@@ -0,0 +1,18 @@
+# Section 2
+# 7. Variables & Variable Types
+
+var1 = 500
+var2 = 45.68
+var3 = "Hello World"
+
+print(var1)
+print(var2)
+print(var3)
+
+a = b = c = 7
+print(a)
+print(b)
+print(c)
+
+print(var3[0:4])
+print(var3[1:])
