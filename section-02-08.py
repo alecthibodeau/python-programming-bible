f883af5bb0a9e424caa540a943868c52cbb54146
alecthibodeau <alecthibodeau@gmail.com>
2020-12-04 06:29:05 -0500

    Complete 2.8 Lists

---
+++ section-02-08.py
@@ -0,0 +1,10 @@
+# Section 2
+# 8. Lists
+
+awesomeList = ['Hello', 45, 'World', 4.5]
+
+print(awesomeList * 8)
+print(awesomeList + awesomeList)
+print(awesomeList[0])
+print(awesomeList[3])
+print(awesomeList[0:2])
