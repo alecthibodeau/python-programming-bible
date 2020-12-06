35cb348eec51b43bf05e8e2bbcc73d079ed59fea
alecthibodeau <alecthibodeau@gmail.com>
2020-12-06 07:48:57 -0500

    Complete 2.14 Assignment Operators

---
+++ section-02-14.py
@@ -0,0 +1,42 @@
+# Section 2
+# 14. Assignment Operators
+
+var1 = 6 + 7
+print(var1)
+
+var1 = var1 + 5
+print(var1)
+
+var1 += 5
+print(var1)
+
+# Compound operators with minus, multiplication, division, modulus, exponent, floor division
+
+var1 -= 5
+print(var1)
+
+var1 *= 5
+print(var1)
+
+var1 /= 5
+print(var1)
+
+var1 %= 5
+print(var1)
+
+var1 **= 5
+print(var1)
+
+var1 //= 5
+print(var1)
+
+# Output
+# 13
+# 18
+# 23
+# 18
+# 90
+# 18
+# 3
+# 243
+# 48
