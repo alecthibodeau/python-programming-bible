30e6104d4d57035755a03b228ad5c4b48ad25118
alecthibodeau <alecthibodeau@gmail.com>
2020-12-06 07:29:24 -0500

    Complete 2.13 Comparison Operators

---
+++ section-02-13.py
@@ -0,0 +1,13 @@
+# Section 2
+# 13. Comparison Operators
+
+print(5 == 6)
+print(5 != 6)
+print(5 > 6)
+print(5 < 6)
+print(5 >= 6)
+print(5 <= 6)
+print(5 > 5)
+print(5 < 5)
+print(5 >= 5)
+print(5 <= 5)
