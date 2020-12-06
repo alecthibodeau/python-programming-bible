7cf63e8b53b5ebff9f40a4cc64ad6c13bdf4a364
alecthibodeau <alecthibodeau@gmail.com>
2020-12-06 07:16:45 -0500

    Complete 2.12 Arithmetic Operators

---
+++ section-02-12.py
@@ -0,0 +1,27 @@
+# Section 2
+# 12. Arithmetic Operators
+
+print(5 + 6)
+print(5 - 6)
+print(5 * 6)
+print(5 / 6)
+
+# Modulus Operator
+print(5 % 6)
+print(9 % 4)
+
+# Exponent Operator
+print(5**2)
+
+# Floor Division
+print(17//3)
+
+# Output
+# 11
+# -1
+# 30
+# 0.8333333333333334
+# 5
+# 1
+# 25
+# 5
