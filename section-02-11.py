f1c095ec4b872407cd2c6e8eecf0d6ba3bd22892
alecthibodeau <alecthibodeau@gmail.com>
2020-12-06 07:02:09 -0500

    Complete 2.11 Data Type Conversion

---
+++ section-02-11.py
@@ -0,0 +1,30 @@
+# Section 2
+# 11. Data Type Conversion
+
+# Convert floating point number to integer
+
+var1 = 6.7
+
+print(var1)
+print(int(var1))
+
+# Experimenting with Data Type Conversion from linked resource:
+# https://www.tutorialspoint.com/python/python_variable_types.htm
+
+var2 = 8
+var3 = "potato"
+
+print(float(var2))
+print(str(var2))
+print(list(var3))
+print(set(var3))
+print(oct(var2))
+
+# Output
+# 6.7
+# 6
+# 8.0
+# 8
+# ['p', 'o', 't', 'a', 't', 'o']
+# {'t', 'p', 'a', 'o'}
+# 0o10
