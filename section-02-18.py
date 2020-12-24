b410402a5b9e22a675f93e319015f2636e3a480a
alecthibodeau <alecthibodeau@gmail.com>
2020-12-24 07:42:55 -0500

    Complete 2.18 Identity Operators

---
+++ section-02-18.py
@@ -0,0 +1,22 @@
+# Section 2
+# 18. Identity Operators
+
+var1 = 10
+var2 = 10
+var3 = 8
+
+# IS Operator
+print(var1 is var2)
+print(var1 is var3)
+
+# IS NOT Operator
+print(var1 is not var2)
+print(var1 is not var3)
+
+# Output
+
+# True
+# False
+
+# False
+# True
