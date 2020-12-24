5c9b91a5445a020afd296265712f099e172c9333
alecthibodeau <alecthibodeau@gmail.com>
2020-12-24 07:13:58 -0500

    Complete 2.17 Membership Operators

---
+++ section-02-17.py
@@ -0,0 +1,18 @@
+# Section 2
+# 17. Membership Operators
+
+string1 = "Hello World"
+
+print('H' in string1)
+print('Z' in string1)
+
+print('H' not in string1)
+print('Z' not in string1)
+
+# Output
+
+# True
+# False
+
+# False
+# True
