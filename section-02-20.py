f750808804687424c115bb04325cb8fe564cd916
alecthibodeau <alecthibodeau@gmail.com>
2020-12-24 10:07:20 -0500

    Complete 2.20 Decision Making

---
+++ section-02-20.py
@@ -0,0 +1,44 @@
+# Section 2
+# 20. Decision Making
+
+# If
+if (500 == 400):
+    print("No")
+if (500 ==500):
+    print("Yes")
+
+# With optional brackets omitted
+if 500 == 400:
+    print("No")
+if 500 == 500:
+    print("Yes")
+
+# Else
+if (500 == 500):
+    print("Yes")
+else:
+    print("No")
+if 500 == 400:
+    print("Yes")
+else:
+    print("No")
+
+# Else If
+var1 = 400
+if (var1 == 500):
+    print("var1 is equal to 500")
+elif (var1 == 400):
+    print("var1 is equal to 400")
+else:
+    print("No it doesn't equal a valid value")
+
+# Single-line decision-making statement
+if (var1 > 50):print("var1 is greater than 50")
+
+# Output
+# Yes
+# Yes
+# Yes
+# No
+# var1 is equal to 400
+# var1 is greater than 50
