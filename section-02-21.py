70c684a02c235eeaf8c3e6b77746ed5316597087
alecthibodeau <alecthibodeau@gmail.com>
2020-12-24 10:27:22 -0500

    Complete 2.21 Loops

---
+++ section-02-21.py
@@ -0,0 +1,43 @@
+# Section 2
+# 21. Loops
+
+# For Loop
+
+var1 = "Hello World"
+
+for character in var1:
+    print(character)
+
+# While Loop
+
+var2 = 100
+
+while (var2 < 110):
+    print("Less than 110")
+    var2 += 1
+else:
+    print("Not less than 110")
+
+# Output
+# H
+# e
+# l
+# l
+# o
+
+# W
+# o
+# r
+# l
+# d
+# Less than 110
+# Less than 110
+# Less than 110
+# Less than 110
+# Less than 110
+# Less than 110
+# Less than 110
+# Less than 110
+# Less than 110
+# Less than 110
+# Not less than 110
