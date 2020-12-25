718a62fa710008bea520965d5aa8870c31d51bfb
alecthibodeau <alecthibodeau@gmail.com>
2020-12-25 13:38:42 -0500

    Complete 2.23 Numbers

---
+++ section-02-23.py
@@ -0,0 +1,20 @@
+# Section 2
+# 23. Numbers
+
+import math # need to import math module
+
+num1 = 5
+
+print(math.exp(num1))
+
+print(math.sqrt(num1))
+
+print(math.sin(math.e)) # built-in e constant
+
+print(math.sin(math.pi/2)) # half-pi is the equivalent of 90 degrees in radians (2-pi is 360 degrees)
+
+#Output
+# 148.4131591025766
+# 2.23606797749979
+# 0.41078129050290885
+# 1.0
