83ea09e2e8e16ca4dd9c70e1d94d382aaa5f9943
alecthibodeau <alecthibodeau@gmail.com>
2020-12-26 16:46:59 -0500

    Complete 2.30 Modules

---
+++ section-02-30.py
@@ -0,0 +1,18 @@
+# Section 2
+# 30. Modules
+
+# Importing/Including a module
+import math
+
+# Specific module functionality importer
+from math import pi # importing the pi constant
+
+print(math.pi)
+
+# The dir() method tries to return a list of valid attributes of the object
+contents = dir(math) # all the methods in the math module
+print(contents)
+
+# Output
+# 3.141592653589793
+# ['__doc__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'ceil', 'copysign', 'cos', 'cosh', 'degrees', 'e', 'erf', 'erfc', 'exp', 'expm1', 'fabs', 'factorial', 'floor', 'fmod', 'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'modf', 'nan', 'pi', 'pow', 'radians', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'tau', 'trunc']
