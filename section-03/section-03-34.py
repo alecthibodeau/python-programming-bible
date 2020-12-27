ec3276b55a71ecda1aa5cab7b724dac74ebfbeac
alecthibodeau <alecthibodeau@gmail.com>
2020-12-27 07:48:01 -0500

    Complete 3.34 Creating Instance Objects

---
+++ section-03/section-03-34.py
@@ -0,0 +1,13 @@
+# Section 3
+# 34. Creating Instance Objects
+
+class Vehicle:
+    speed = 0
+
+    def IncreaseSpeed(self, increaseAmount):
+        self.speed += increaseAmount
+
+car1 = Vehicle()
+car2 = Vehicle()
+
+# Output (none)
