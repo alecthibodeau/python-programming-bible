7085d56fd59e15a97d566fe37fe36549db988b76
alecthibodeau <alecthibodeau@gmail.com>
2020-12-27 07:44:19 -0500

    Complete 3.33 Simple Example

---
+++ section-03/section-03-33.py
@@ -0,0 +1,10 @@
+# Section 3
+# 33. Simple Example
+
+class Vehicle:
+    speed = 0
+
+    def IncreaseSpeed(self, increaseAmount): # self keyword passes in the instance of the class itself
+        self.speed += increaseAmount
+
+# Output (none)
