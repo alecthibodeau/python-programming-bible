8a5120ea9cb86e823b4b0aa6ffece11cb8ea59ad
alecthibodeau <alecthibodeau@gmail.com>
2020-12-27 10:31:13 -0500

    Complete 3.39 Overriding Methods

---
+++ section-03/section-03-39.py
@@ -0,0 +1,62 @@
+# Section 3
+# 39. Overriding Methods
+
+class Vehicle:#(object):
+    speed = 0
+
+    # This is the constructor for Python, which you generally won't use
+    # def __new__(cls):
+    #     return object.__new__(cls)
+
+    # This is the initialization method, not the constructor
+    # It's called after the class has been created and has access to all the variables and methods
+    # Every time we create a class it'll call the constructor and then the intialization method
+    def __init__(self, speed = 0):
+        self.speed = speed # assigning to the local variable
+
+    def IncreaseSpeed(self, increaseAmount):
+        self.speed += increaseAmount
+
+    def __del__(self):
+        print("Object has been destroyed")
+
+class Car(Vehicle): # Car class is inheriting from Vehicle
+    weight = 10
+
+    def IncreaseWeight(self, weight):
+       self.weight += weight
+
+    def IncreaseSpeed(self, increaseAmount):
+        self.speed *= increaseAmount
+
+car1 = Vehicle()
+car2 = Vehicle(89)
+# Speed is set to 5 on creating childCar
+childCar = Car(5)
+
+print(childCar.weight)
+childCar.IncreaseWeight(9)
+print(childCar.weight)
+# Increase speed in the child class, not the parent class, which multiplies rather than adds
+childCar.IncreaseSpeed(5)
+print(childCar.speed)
+
+print("Speed for car 1: %i" % (car1.speed))
+print("Speed for car 2: %i" % (car2.speed))
+car1.IncreaseSpeed(5)
+print("Speed for car 1: %i" % (car1.speed))
+print("Speed for car 2: %i" % (car2.speed))
+
+del car1
+
+# Output
+# 10
+# 19
+# 25
+# Speed for car 1: 0
+# Speed for car 2: 89
+# Speed for car 1: 5
+# Speed for car 2: 89
+# Object has been destroyed
+# Object has been destroyed
+# Object has been destroyed
